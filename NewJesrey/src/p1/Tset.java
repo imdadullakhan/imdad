d03414b6e1f30d23d0a4c70b44f2b06993fb085f
sdsamdani <matin@DESKTOP-UQ13F0U>
2019-07-01 22:06:06 +0530

    sysout is added

--- NewJesrey/src/p1/Tset.java
+++ NewJesrey/src/p1/Tset.java
@@ -5,7 +5,8 @@ public class Tset {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(" add the project to git");
+		System.out.println(" add the project");
+		System.out.println(" another class is added ");
 
 	}
 
