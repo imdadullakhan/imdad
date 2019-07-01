eb6c84f845eae49a758040a4398a96d3f7ab471d
sdsamdani <matin@DESKTOP-UQ13F0U>
2019-07-01 21:34:58 +0530

    new project

--- NewJesrey/src/p1/Tset.java
+++ NewJesrey/src/p1/Tset.java
@@ -5,7 +5,7 @@ public class Tset {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(" add the project");
+		System.out.println(" add the project to git");
 
 	}
 
